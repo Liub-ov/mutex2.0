ba418176cf6758a50a1f12d55df097508f62e83e
Liub-ov <79312905+Liub-ov@users.noreply.github.com>
2021-06-03 16:17:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace ConsoleApp6
+{
+    public static class Program
+    {
+        const string mutex_id = "Global\\{a1b2c3d4e5}";
+
+        public static int Main()
+        {
+            var threadNmae = Thread.CurrentThread.Name;
+            Console.WriteLine($"Started by thread: {threadNmae}");
+            using (var mutex = new Mutex(false, mutex_id))
+            {
+                    try
+                    {
+                        if (!mutex.WaitOne(TimeSpan.FromSeconds(5), false))
+                        {
+                            Console.WriteLine("Another instance of this program is running");
+
+                            return -1;
+                        }
+                        else
+                        {
+                            var seconds = 5;
+                            Console.WriteLine($"wait for {seconds} seconds");
+                            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+
+                            return 0;
+                        }
+                    }
+                    catch (AbandonedMutexException e)
+                    {
+                        Console.WriteLine($"Mutex '{mutex_id}' exception appeared:\n", e.Message);
+                        throw e;
+                    }
+            }
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp6;
+using System.Threading;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestSeveralThreads()
+        {
+            int value = 1;
+            var thread1 = new Thread(() => value = Program.Main());
+            thread1.Name = "Thread 1";
+            var thread2 = new Thread(() => value = Program.Main());
+            thread2.Name = "Thread 2";
+            var thread3 = new Thread(() => value = Program.Main());
+            thread3.Name = "Thread 3";
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+
+            Console.WriteLine($"Result: {value}");
+
+            Assert.AreEqual(-1, value);
+        }
+
+        [TestMethod]
+        public void TestSingleThread()
+        {
+            int value = 1;
+            var thread1 = new Thread(() => value = Program.Main());
+            thread1.Name = "Thread 1";
+            thread1.Start();
+            thread1.Join();
+
+            Console.WriteLine($"Result: {value}");
+
+            Assert.AreEqual(0, value);
+        }
+    }
+}
